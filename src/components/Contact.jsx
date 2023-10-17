6a1a88d74bf56c1889044c94efda0b3125c4ce5b
alexandermihailovich <xanderodens@gmail.com>
2023-10-17 11:57:36 +0300

    fix button (.btn) hover effect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,6 @@ function Contact() {
 					<div className="contact__buttons-group">
 						<div className="contact__btn">
 							<a className="btn" href="https://www.linkedin.com/in/alexkuleshov-frontend/" target="_blank" rel="noreferrer">
-								<span className="btn__bg"></span>
 								<span className="btn__text">
 									Linkedin&nbsp;<ExternalLinkIcon />
 								</span>
@@ -19,7 +18,6 @@ function Contact() {
 						</div>
 						<div className="contact__btn">
 							<a className="contact__btn btn" href="mailto:amkuleshov@gmail.com">
-								<span className="btn__bg"></span>
 								<span className="btn__text">
 									Email
 								</span>
